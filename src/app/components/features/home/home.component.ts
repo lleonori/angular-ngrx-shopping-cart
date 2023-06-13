c980e41c42a0576ee3929848e5601087b824fa86
lorenzo.leonori <lorenzo.leonori@par-tec.it>
2023-06-13 14:35:10 +0200

    T0; test eslint

--- src/app/components/features/home/home.component.ts
+++ src/app/components/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { CartItem } from 'src/app/core/models/global.models';
@@ -9,12 +9,12 @@ import { selectCartItems, selectCartTotalPrice, selectNumberOfItems } from 'src/
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
   numberOfItems$: Observable<number> = this.store.select(selectNumberOfItems);
   totalPrice$: Observable<number> = this.store.select(selectCartTotalPrice);
   cartItems$: Observable<CartItem[]> = this.store.select(selectCartItems);
 
   constructor(private store: Store) {}
 
-  ngOnInit(): void {}
+
 }
